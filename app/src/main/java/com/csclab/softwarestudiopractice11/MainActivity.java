be58ad6c371ebfa06f2bbe4cf1443774e5a889d9
Liven Pan <helen36974@gmail.com>
2016-05-26 18:06:26 +0800

    Update all

--- app/src/main/java/com/csclab/softwarestudiopractice11/MainActivity.java
+++ app/src/main/java/com/csclab/softwarestudiopractice11/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private Button send;
     private EditText edt;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
